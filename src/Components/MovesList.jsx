9f067e14320246dc59d9dd355ff7dcf3c56dd346
Lisandro Enrici <lisandroenrici@gmail.com>
2023-07-14 16:22:56 +0200

    Jump to step action done

--- src/Components/MovesList.jsx
+++ src/Components/MovesList.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { actions } from '../actions';
 
 function MovesList() {
     const history = useSelector(state => state.history);
+    const dispatch = useDispatch()
 
     const moves = history.map((step, move) => {
         const desc = move ? "Go to move # " + move : "Restart Game";
@@ -20,7 +22,7 @@ function MovesList() {
     });
     
     function jumpTo(step) {
-        console.log(step);
+        dispatch(actions.goToStep(step));
     };
     
     return (
