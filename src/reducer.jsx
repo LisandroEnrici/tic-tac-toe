52c7715873311d849825443690cb8ffba9037479
Lisandro Enrici <lisandroenrici@gmail.com>
2023-07-14 16:42:29 +0200

    end game action done

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -11,7 +11,25 @@ const initialState = {
     ]
 };
 
-
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
@@ -19,25 +37,23 @@ const reducer = (state = initialState, action) => {
             const history = state.history.slice(0, state.stepNumber + 1 );
             const current = history[state.stepNumber];
             const squaresCopy = current.squares.slice();
-
-            squaresCopy[action.square] = state.xIsNext ? 'X' : 'O'
+            
+            squaresCopy[action.square] = state.xIsNext ? 'X' : 'O';
+            const winner = calculateWinner(squaresCopy);
             return {
                 ...state,
                 history: history.concat([{
                     squares: squaresCopy
                 }]),
                 xIsNext: !state.xIsNext,
-                stepNumber: history.length
+                stepNumber: history.length,
+                winner
             };
         case actionTypes.goToStep:
             return {
                 ...state,
                 stepNumber: action.step,
                 xIsNext: (action.step % 2) === 0,
-            };
-        case actionTypes.gameOver:
-            return {
-                ...state
             };
         default:
             return state;
