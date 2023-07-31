d00b1bb0d565d63381ccdabd0a6259a07d4a4208
Lisandro Enrici <lisandroenrici@gmail.com>
2023-07-31 13:34:58 +0200

    Game container styles done

--- src/Containers/GameContainer.jsx
+++ src/Containers/GameContainer.jsx
@@ -3,14 +3,41 @@ import Board from './Board.jsx';
 import StatusBar from '../Components/StatusBar.jsx';
 import MovesList from '../Components/MovesList.jsx';
 
+const styles = {
+    gameContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '90%',
+        maxWidth: '30rem',
+        height: '90%',
+        maxHeight: '15rem',
+        color: '#fff',
+        textAlign: 'center',
+        boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0.25rem 1.5rem',
+        border: '1px solid rgba( 255, 255, 255, 0.18 )',
+        borderRadius: '1.5rem',
+        background: 'radial-gradient(at top left, rgba(255,255,255,0.3) 0%, rgba(255,255,255,0) 100%) border-box',
+        backdropFilter: 'blur(3px)',
+    },
+
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'start',
+        width: '50%',
+        height: '100%'
+    }
+}
+
 function GameContainer() {
 
     return (
-        <div className='game'>
-            <div className='board-game'>
+        <div style={styles.gameContainer}>
+            <div style={styles.column}>
                 <Board />
             </div>
-            <div className='game-info'>
+            <div style={styles.column}>
                 <StatusBar />
                 <MovesList />
             </div>
